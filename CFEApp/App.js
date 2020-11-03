d333b7bc1d30c64fd39ee2fe68e02f202bccb029
zenoheld1994 <luis.ben.94@gmail.com>
2020-11-03 11:22:25 -0600

    login

--- CFEApp/App.js
+++ CFEApp/App.js
@@ -1,114 +1,51 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {StyleSheet, View,Switch, Text,StatusBar,Image,TextInput} from 'react-native';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+import picLogo from './assets/appLogo.jpeg';
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+class  CFEApp extends React.Component {
+
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden= {true} />
+        <Image source={picLogo} />
+        <Text>correo electronico </Text>
+        <TextInput defaultValue="correo"/>
+        <Text>contraseña</Text>
+        <TextInput defaultValue="pwd"/>
+        <Text>¿No tienes cuenta?</Text>
+        <Text>Registrate aquí</Text>
+        
+        
+      </View>
+    );
+  }
+}
+
+
+const styles= StyleSheet.create({
+  container:{
+    flex:1,//flex of 1 means the whole screen
+    flexDirection: 'column',
+    justifyContent:'space-around',//'space-around'
+    alignItems:'flex-start',
+    backgroundColor: '#DDD'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  defaultText:{
+    fontSize:22,
+    padding:10,
+    margin: 5,
+    color: 'black'
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  selectedText:{
+    //alignSelf: 'flex-start'
+    backgroundColor: 'yellow',
+    color: 'forestgreen',
+    fontWeight:'bold',
+  }
 });
 
-export default App;
+export default CFEApp;
